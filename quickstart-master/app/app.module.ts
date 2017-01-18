c6f5b705dc131dfbe9ff902a7947ce22500aba4e
91Seconds <king.ciaran72@gmail.com>
2017-01-19 02:31:41 +1300

    intergrated the hexagon grid into angular app, still not a component

--- quickstart-master/app/app.module.ts
+++ quickstart-master/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { HexagonTileComponent } from './hexagon-tile.component'
+import { HexagonLayoutComponent } from './hexagon-layout.component'
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ 
     AppComponent,
-    HexagonTileComponent
+    HexagonTileComponent,
+    HexagonLayoutComponent
   ],
   bootstrap:    [ AppComponent ]
 })
---
+++ quickstart-master/app/hexagon-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-hexagon-layout',
+  templateUrl: 'hexagon-layout.component.html',
+  styleUrls: [ 'hexagon-layout.component.css' ]
+})
+
+export class HexagonLayoutComponent  { 
+    name: string;
+    color: string;
+}
